417eabb6f77a6e7b3d16c6fc09bd36fd2ffd94d7
156 <bw1203j@naver.com>
2023-12-22 20:42:21 +0900

    codeup basic 100제 1~18

---
+++ dogorcat/기초100제.py
@@ -0,0 +1,79 @@
+# 1번
+# print("Hello")
+
+# 2번
+# print("문장1 문장2")
+# print("문장1","문장2")
+
+# 3번
+# print("Hello")
+# print("World")
+
+# 4번
+# print("'Hello'")
+
+# 5번
+#print ("'"'HelloWorld'"'")
+
+# 6번
+# print ("\"!@#$%^&*()\'")
+
+# 7번
+# print ("\"C:\Download\\'hello\'.py\"")
+
+# 8번
+# print ("Hello\nWorld")
+
+# 9번
+# c=input()
+# print(c)
+
+# 10번
+# n = input()
+# n = int(n)
+# print(n)
+
+# 11번
+# f = input()
+# f = float(f)
+# print(f)
+
+# 12번
+# a = input()
+# b = input()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+
+# 13번
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+# 14번
+# a = input()
+# a = float(a)
+# print(a)
+# print(a)
+# print(a)
+
+# 15번
+# a, b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+
+# 16번
+# a, b = input().split()
+# print(b, a)
+
+# 17번
+# s = input()
+# print(s, s, s)
+
+# 18번
+y, m, d = input().split('.')
+print(d,m,y,sep='-')
